59141c24e29b907a829a08538d3d5d77445901b2
ledangkhanhdut <khanhdangabc2@gmail.com>
2024-10-18 09:50:47 +0700

    test.cpp

--- test.cpp
+++ test.cpp
@@ -3,7 +3,7 @@
 
 int main(){
     Account p ;
-    cout<< p.Login() << endl; // true or false 
+    //cout<< p.Login() << endl; // true or false 
     cout<< p.Register("nguyendinhien","hien2200",'m'); //tra ve true false
     return 0;
 }
