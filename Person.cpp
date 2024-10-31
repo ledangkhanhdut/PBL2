7afef1d42a71f578af5b09798f9f3a0c28fba1a9
hwolp <138670303+hwolp@users.noreply.github.com>
2024-10-31 23:12:12 +0700

    Person.cpp

--- Person.cpp
+++ Person.cpp
@@ -1,30 +1,33 @@
 #include "Person.h"
 
-Person::Person(const string &name, int age, const string &phone, const string &address, char gender)
-{
-    this->name = name;
+Person::Person(unsigned int Id,const string &name, int age, const string &phone, const string &address, char gender)
+{   
+    this->Id = Id;
+    this->Name = Name;
     this->age = age;
     this->Phone_Number = phone;
     this->address = address;
     this->gender = gender;
 }
+
 Person::Person(const Person &p)
-{
-    this->name = p.name;
+{   
+    this->Id = p.Id;
+    this->Name = p.Name;
     this->age = p.age;
     this->Phone_Number = p.Phone_Number;
     this->address = p.address;
     this->gender = p.gender;
 }
 
-void Person::update_Infomation(const Person &p)
+void Person::update_Information(const Person &p)
 {
-    this->name = p.name;
+    this->Id = p.Id;
+    this->Name = p.Name;
     this->age = p.age;
     this->Phone_Number = p.Phone_Number;
     this->address = p.address;
     this->gender = p.gender;
 }
-Person::~Person()
-{
+Person::~Person(){
 }
