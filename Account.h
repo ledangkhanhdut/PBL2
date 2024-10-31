7d14f4ffcf66b539a77fe24d926b386c741faf3f
hwolp <138670303+hwolp@users.noreply.github.com>
2024-10-31 23:09:43 +0700

    Account.h

--- Account.h
+++ Account.h
@@ -10,15 +10,17 @@ private:
   int ID;
   string Username;
   string Pass;
-  char Role;
+  int Role;
 
 public:
-  Account(const int & = -1, const string & = "", const string & = "", char = 'P');
+  Account(const int & = -1, const string & = "", const string & = "", int = 4);
   Account(const Account &);
-  bool Register(const string &, const string &, char);
-  int Login();
+  bool Register(const string &, const string &, int);
+  bool Login(const string &, const string &);
   void Change_Pass(const string &);
   bool Compare(const string &, const string &);
+  bool Remove_Acc(const string &);
   int get_Id();
+  int get_Role();
   ~Account();
 };
